215fc31669d59c57d1b52cce5db6026a7e336369
Jose Barreto <joseb1997@hotmail.com>
2022-05-20 12:56:45 -0300

    Creación del proyecto (Spring y .gitignore)

---
+++ src/main/java/ar/edu/utn/link/tpIntegradorLink/TpIntegradorLinkApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.link.tpIntegradorLink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpIntegradorLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpIntegradorLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/utn/link/tpIntegradorLink/TpIntegradorLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.link.tpIntegradorLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpIntegradorLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
