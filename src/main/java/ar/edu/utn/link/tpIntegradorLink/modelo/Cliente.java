3b1c78cd302a020dc800806faa41104166a5b30d
Jose Barreto <joseb1997@hotmail.com>
2022-05-20 15:43:48 -0300

    Creacion de algunas Clases del Dominio

---
+++ src/main/java/ar/edu/utn/link/tpIntegradorLink/modelo/Cliente.java
@@ -0,0 +1,19 @@
+package ar.edu.utn.link.tpIntegradorLink.modelo;
+
+import java.time.LocalDate;
+import java.util.Collection;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Cliente {
+	private String apellido;
+	private String nombre;
+	private LocalDate fechaNacimiento;
+	private String nroDocumento;
+	private String telefono;
+	private String mail;
+	private Collection<OrdenCompra> comprasEfectuadas;
+	
+}
